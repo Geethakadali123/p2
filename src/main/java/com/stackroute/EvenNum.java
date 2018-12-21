94cfbd79e0cb2bdfb889e6edb8c87dc9accc0503
kranthichallagulla <kranthichallagulla>
2018-12-21 14:02:32 +0530

    modified even

---
+++ src/main/java/com/stackroute/EvenNum.java
@@ -0,0 +1,9 @@
+package com.stackroute;
+
+public class EvenNum {
+    public static boolean isEven(int n){
+
+        return true;
+    }
+}
+
--- src/test/java/com/stackroute/EvenNumTest.java
+++ src/test/java/com/stackroute/EvenNumTest.java
@@ -2,12 +2,20 @@ package com.stackroute;
 import org.junit.Test;
 import  org.junit.Assert.*;
 
+import static org.junit.Assert.assertEquals;
 
 
 public class EvenNumTest {
     @Test
-    public void EvenCheck(){
-        boolean result = isEven(10);
-        assertEquals(true,result);
+    public void Evencheck(){
+        assertEquals(true,EvenNum.isEven(2));
+    }
+    @Test
+    public void Eventest(){
+        assertEquals(true,EvenNum.isEven(24));
+    }
+    @Test
+    public void Noteven(){
+        assertEquals(false,EvenNum.isEven(13));
     }
 }
